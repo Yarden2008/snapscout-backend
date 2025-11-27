95ba4576184afe5ab1d5ca19e6cfbbc22b2b6ff4
Yarden2008 <152523290+Yarden2008@users.noreply.github.com>
2025-11-27 10:37:47 +0200

    Update onnx_inference.py

--- onnx_inference.py
+++ onnx_inference.py
@@ -10,6 +10,10 @@ session = ort.InferenceSession("mobilenetv2-7.onnx", providers=["CPUExecutionPro
 IMG_SIZE = 224
 LABELS = [line.strip() for line in open("labels.txt", "r").readlines()]
 
+shape = session.get_outputs()[0].shape
+print("OUTPUT SHAPE =", shape)
+
+
 def preprocess(img: Image.Image):
     img = img.resize((IMG_SIZE, IMG_SIZE))
     arr = np.array(img).astype("float32") / 255.0
