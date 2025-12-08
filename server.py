60bb9db046e90b7adb7b6df8e9420ea49912a4df
Yarden2008 <152523290+Yarden2008@users.noreply.github.com>
2025-12-08 15:14:39 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,27 +1,31 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
-from onnx_inference import classify_image
 from PIL import Image
 import io
+
+from onnx_inference import classify_image
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"status": "mobilenetv2-10 backend running"}
+    return {"status": "EfficientNet-Lite4 backend running"}
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
     try:
-        content = await file.read()
-        image = Image.open(io.BytesIO(content)).convert("RGB")
+        contents = await file.read()
+        image = Image.open(io.BytesIO(contents)).convert("RGB")
 
-        label, score = classify_image(image)
+        label, confidence = classify_image(image)
 
         return {
             "label": label,
-            "confidence": round(score, 4)
+            "confidence": confidence
         }
 
     except Exception as e:
-        return JSONResponse({"error": str(e)}, status_code=500)
+        return JSONResponse(
+            {"error": str(e)},
+            status_code=500
+        )
